c0346bb2254eae3d550709786d029122375d6db2
Giuliana De Bellis <z5309331@unsw.edu.au>
2022-10-08 11:58:55 +1100

    Iteration 2 almost ready - proof read needed

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import express, { json, Request, Response } from 'express';
+import { echo } from './echo';
+import morgan from 'morgan';
+import config from './config.json';
+import cors from 'cors';
+
+// Set up web app
+const app = express();
+// Use middleware that allows us to access the JSON body of requests
+app.use(json());
+// Use middleware that allows for access from other domains
+app.use(cors());
+
+const PORT: number = parseInt(process.env.PORT || config.port);
+const HOST: string = process.env.IP || 'localhost';
+
+// Example get request
+app.get('/echo', (req: Request, res: Response, next) => {
+  try {
+    const data = req.query.echo as string;
+    return res.json(echo(data));
+  } catch (err) {
+    next(err);
+  }
+});
+
+// for logging errors (print to terminal)
+app.use(morgan('dev'));
+
+// start server
+const server = app.listen(PORT, HOST, () => {
+  // DO NOT CHANGE THIS LINE
+  console.log(`⚡️ Server listening on port ${PORT} at ${HOST}`);
+});
+
+// For coverage, handle Ctrl+C gracefully
+process.on('SIGINT', () => {
+  server.close(() => console.log('Shutting down server gracefully.'));
+});
