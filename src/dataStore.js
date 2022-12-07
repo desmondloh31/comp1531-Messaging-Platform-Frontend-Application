620db09d56e81ab9997b9c58730f36676f359689
Giuliana De Bellis <z5309331@unsw.edu.au>
2022-12-07 11:59:55 +1100

    datastore

--- src/dataStore.js
+++ src/dataStore.js
@@ -23,6 +23,9 @@ function getData() {
 }
 
 // Use set(newData) to pass in the entire data object, with modifications made
+// - Only needs to be used if you replace the data store entirely
+// - Javascript uses pass-by-reference for objects... read more here: https://stackoverflow.com/questions/13104494/does-javascript-pass-by-reference
+// Hint: this function might be useful to edit in iteration 2
 function setData(newData) {
   data = newData;
 }
