f5c2d55b7ce9662542b863cd6fc85add905b4278
Allen Sheng <z5328002@ad.unsw.edu.au>
2023-02-23 10:41:05 +1100

    adding the authLoginV1 function

--- src/auth.js
+++ src/auth.js
@@ -1,13 +1,5 @@
-//This is a test file atm
-
 function authLoginV1(email, password) {
     return {
       authUserId: 1,
     }
   }
-
-function authRegisterV1(email, password, nameFirst, nameLast) {
-    return{
-        authUserId: 1,
-    }
-}
