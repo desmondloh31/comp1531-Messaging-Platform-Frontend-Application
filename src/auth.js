c19281cd81b151b7e5fffad44970236ca7f2fd22
Allen Sheng <z5328002@ad.unsw.edu.au>
2023-02-23 10:50:29 +1100

    adding authRegisterV1 function

--- src/auth.js
+++ src/auth.js
@@ -3,3 +3,9 @@ function authLoginV1(email, password) {
       authUserId: 1,
     }
   }
+
+function authRegisterV1(email, password, nameFirst, nameLast) {
+    return{
+        authUserId: 1,
+    }
+}
