330dd0288e975e0ed85ac885362395c0f35a9e66
Kavish <z5257649@ad.unsw.edu.au>
2023-02-23 11:43:34 +1100

    Creating channelDetailsV1 stub function

--- src/channel.js
+++ src/channel.js
@@ -1,1 +1,24 @@
 
+function channelDetailsV1(authUserId, channelId){
+    return{
+        name: 'Hayden',
+        ownerMembers: [
+          {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+          }
+        ],
+        allMembers: [
+          {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+          }
+        ],
+      }
+}
