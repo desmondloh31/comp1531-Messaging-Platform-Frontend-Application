f2e10f10ef8579931c1cf1b3169ed97d355f9920
Jin Wu Desmond Loh <z5324992@ad.unsw.edu.au>
2023-02-23 11:24:15 +1100

    added chanelsListV1

--- src/channels.js
+++ src/channels.js
@@ -1,5 +1,18 @@
+
 function channelsCreatev1(authUserId, name, isPublic) {
     return {
         channelId: 1
     }
 }
+
+function channelsListV1(authUserId) {
+    return {
+        channels: [
+            {
+                channelId: 1,
+                name: "stub Channel",
+            }
+        ],
+    }
+}
+    
