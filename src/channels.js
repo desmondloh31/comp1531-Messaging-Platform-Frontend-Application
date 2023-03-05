a4da9ebb3e19779747c21a2e113a892cd44a54e5
Jin Wu Desmond Loh <z5324992@ad.unsw.edu.au>
2023-03-06 09:45:40 +1100

    made changes to channelsCreate and channelsList functions

--- src/channels.js
+++ src/channels.js
@@ -30,26 +30,37 @@ function channelsListAllV1(authUserId) {
 function channelsCreateV1(authUserId, name, isPublic) {
 
   const data = getData();
+
+  //check if authUserId is valid:
+  const user = data.users[authUserId];
+  if (!user) {
+    return { error: "authUserId is invalid"};
+  }
+
+  //check if name is valid:
   if (name.length < 1 || name.length > 20) {
-    return {error: "error"};
+    return { error: "length of name is less than 1 or more than 20 characters"};
   }
 
-  if (!data.users[authUserId]) {
-    return {error: "error"};
-  }
+  //create a new channel object:
+  const newChannel = {
+    channelId: uuid(),
+    name: name,
+    isPublic: isPublic,
+    members: [authUserId],
+  };
 
-  const channelId = uuid();
-  data.channels[channelId] = {
-    name,
-    isPublic,
-    userIds: [authUserId]
-  }
-
+  //add a new channel to the data store:
+  data.channels[newChannel.channelId] = newChannel;
   setData(data);
 
   return {
-    channelId
-  }
+    channelId: newChannel.channelId,
+    name: newChannel.name,
+    isPublic: newChannel.isPublic,
+    members: newChannel.members,
+  };
+
 
 }
 
@@ -57,23 +68,23 @@ function channelsCreateV1(authUserId, name, isPublic) {
 function channelsListV1(authUserId) {
 
   const data = getData();
-  if (!data.users[authUserId]) {
-    return {error: "error"};
+
+  //check if authUserId is valid:
+  const user = data.users[authUserId];
+  if (!user) {
+    return { error: "authUserId is invalid"};
   }
 
+  //get array of channels that the user is a member of:
   const channels = [];
-  for (const channel of Object.values(data.channels)) {
-    if (channel.userIds.includes(authUserId)) {
-      channels.push ({
-        channelId: channel.channelId,
-        name: channel.name
-      })
+  for (const channelId in data.channels) {
+    const channel = data.channels[channelId];
+    if (channel.members.includes(authUserId)) {
+      channels.push(channel);
     }
   }
-
-  return {
-    channels
-  }
+  
+  return { channels: channels};
       
 }
     
