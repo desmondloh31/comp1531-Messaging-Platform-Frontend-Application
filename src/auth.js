6e507da75d30d9fe1b25bf9b72f251f984214861
Allen Sheng <z5328002@ad.unsw.edu.au>
2023-03-05 13:11:48 +1100

    adding incomplete but usable auth functions

--- src/auth.js
+++ src/auth.js
@@ -1,12 +1,54 @@
+import {getData, setData} from './dataStore';
+import { uuid } from 'uuidv4';
+
 function authRegisterV1(email, password, nameFirst, nameLast) {
+    const { uuid } = require('uuidv4');
+    const data = getData()
+    const authUserId = uuid()
+    data.users["email"] = {
+        email
+    }
+    data.users["password"] = {
+        password
+    }
+    data.users["nameFirst"] = {
+        nameFirst
+    }
+    data.users["nameLast"] = {
+        nameLast
+    }
+    data.users["authUserId"] = {
+        authUserId
+    }
+    
+    setData(data)
     return{
-        authUserId: 1,
+        authUserId
     }
   }
 
 
 function authLoginV1(email, password) {
-    return{
-        authUserId: 1,
+
+    const data = getData()
+    for (const user in data){
+        if (findEmail(user["email"], email)){
+            if(user["password"] == password){
+                return user["authUserId"]
+            } 
+            else{
+                return {error:"error"}
+            }
+        }
     }
 }
+
+
+function findEmail(useremail, email){
+    if (useremail == email){
+
+        return True
+    }
+}
+
+export {authRegisterV1, authLoginV1};
