a3abfdefdb3c1620604ee6cc0dc9ad1d0760bede
Kavish <z5257649@ad.unsw.edu.au>
2023-03-09 19:48:30 +1100

    Addings tests again :(

---
+++ src/channel.test.js
@@ -0,0 +1,58 @@
+import {channelDetailsV1, channelMessagesV1, channelInviteV1,channelJoinV1} from './channel.js';
+import {getData, setData } from './dataStore.js';
+
+//testing channelDetails:
+describe ("Testing channelDetails", () => {
+    const authUserId = 'user123';
+    const ownerMemberId = 'owner123'
+    const channel1 = {
+        channelId: 'channel1',
+        name: 'channel1a',
+        isPublic: true,
+        memberIds: [authUserId],
+        ownerIds: [ownerMemberId],
+    };
+
+    const channel2 = {
+        channelId: 'channel2',
+        name: 'channel2a',
+        isPublic: false,
+        memberIds: ['otherUser'],
+        ownerIds: [ownerMemberId],
+    };
+
+    beforeEach (() => {
+        const data = {
+            users: { [authUserId]: {} },
+            channels: { [channel1.channelId]: channel1, [channel2.channelId]: channel2},
+            
+        };
+        setData(data);
+    });
+   
+    test ('testing if authUserId is not valid', () => {
+        const result = channelDetailsV1('asdas','channel1');
+        expect(result).toEqual({error: "authUserId is invalid"});
+    });
+    test ('testing if authUserId is valid', () => {
+        const result = channelDetailsV1(authUserId,'channel1');
+        expect(result).toEqual(getData().channels.channel1);
+    });
+    test ('testing if channelId is valid', () => {
+        const result = channelDetailsV1(authUserId,'channel1');
+        expect(result).toEqual(getData().channels.channel1);
+    });
+    test ('testing if channelId is not valid', () => {
+        const result = channelDetailsV1(authUserId,'invalidChannelName');
+        expect(result).toEqual({error: 'Invalid channel Name'});
+    });
+    test ('authUser is not a part of the channel', () => {
+        const result = channelDetailsV1(authUserId,'channel2');
+        expect(result).toEqual({error: 'User is not a part of the channel'});
+    });
+    test ('authUser is a part of the channel', () => {
+        const result = channelDetailsV1(authUserId,'channel1');
+        expect(result).toEqual(getData().channels.channel1);
+    });
+
+});
