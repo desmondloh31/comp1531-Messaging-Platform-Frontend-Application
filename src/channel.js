49146079b0e23ce37155013a4a4e84d5cd66af3a
Kavish <z5257649@ad.unsw.edu.au>
2023-03-09 12:02:24 +1100

    initial code

--- src/channel.js
+++ src/channel.js
@@ -1,7 +1,43 @@
 
+import {getData, setData} from './dataStore.js';
 
-function channelDetailsV1(authUserId, channelId){
-    return{
+export function channelDetailsV1(authUserId, channelId){
+  
+  //Determining whether authUserId is valid
+  const data = getData();
+  const user = data.users[authUserId];
+  if (!user) {
+    return { error: "authUserId is invalid"}; 
+  }
+
+  if(data.channels == false){
+    return {error: 'User is not a part of the channel'};
+  }
+
+
+  //Determine whether the channelId is valid & user is a part of the channel
+  for (const currentChannelId in data.channels) {
+    const channel = data.channels[currentChannelId];
+    if(currentChannelId == channelId && (channel.ownerIds.includes(authUserId)||channel.memberIds.includes(authUserId))){
+      return  channel; 
+    }
+  }
+
+  
+  if(data.channels.find(channelId=>channelId = channelId) == false){
+    return {error: 'Invalid channel Name'};
+  }
+
+  //Check whether the authorised user is a part of the channel
+
+  return{error: 'User is not a part of the channel'}
+ 
+
+  
+  
+
+  
+  return{
         name: 'Hayden',
         ownerMembers: [
           {
