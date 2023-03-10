20c0d1a274b62189ed8f32c3d9fa65b7b972f48b
Hritwik Nauriyal <z5425235@ad.unsw.edu.au>
2023-03-10 14:54:38 +1100

    tests for channelMessage

--- src/channel.test.js
+++ src/channel.test.js
@@ -1,5 +1,8 @@
 import {channelDetailsV1, channelMessagesV1, channelInviteV1,channelJoinV1} from './channel.js';
 import {getData, setData } from './dataStore.js';
+import { clearV1 } from "./clearV1";
+import { authRegister } from './auth.js';
+import { channelsCreateV1 } from './channels.js';
 
 //testing channelDetails:
 describe ("Testing channelDetails", () => {
@@ -56,3 +59,56 @@ describe ("Testing channelDetails", () => {
     });
 
 });
+
+//testing channelMessages:
+
+
+
+const ERROR = { error: expect.any(String) };
+
+describe('Error Checking in channel messages v1', () => {
+    beforeEach (() => {
+        clearV1();
+        authRegister("test@gmail.com", "test", "test", "test");
+    });
+
+    test('invalid authuser id', () => {
+        let userid = -1;
+        let channelid = 0;
+        const result = channelMessagesV1(userid, channelid, 0);
+        expect(result).toStrictEqual(ERROR);
+    });
+
+    test('invalid channel id', () => {
+        let userid = 0;
+        let channelid = -1;
+        const result = channelMessagesV1(userid, channelid, 0);
+        expect(result).toStrictEqual(ERROR);
+    });
+
+    test('invalid start', () => {
+        let userid = 0;
+        let channelid = 0;
+        const result = channelMessagesV1(userid, channelid, 1);
+        expect(result).toStrictEqual(ERROR);    
+    });
+
+    test('Authuser not part of channel', () => {
+        let userid = 1;
+        let channelid = 0;
+        const result = channelMessagesV1(userid, channelid, 0);
+        expect(result).toStrictEqual(ERROR);
+    });
+
+    test('Valid Test', () => {
+        let userid = 0;
+        let channelid = 0;
+        const result = channelMessagesV1(userid, channelid, 0);
+        expect(result).toStrictEqual({messages: [],start: 0, end: -1});
+    });
+
+});
+
+
+
+
