88cb45a92dceb9cc14b6b9ad036edec4176ab8e2
Hritwik Nauriyal <z5425235@ad.unsw.edu.au>
2023-03-10 21:21:33 +1100

    last eduit

---
+++ src/users.js
@@ -0,0 +1,22 @@
+export function userProfileV1(authUserID, uId) {
+
+    const data = getData();
+    
+    const user = data.users.find(i => i.authUserId == authUserId);
+    const guest = data.users.find(i => i.authUserId == uId);
+  
+    if (!user) {
+        return { error: "authUserId is invalid"}; 
+      }else if(!guest) {
+        return { error: "uId is invalid"};
+      }
+
+    return {
+        uId: guest.uId,
+        nameFirst: guest.nameFirst,
+        nameLast: guest.nameLast,
+        email: guest.email,
+        handleStr: guest.handleStr,
+
+    }
+  }
