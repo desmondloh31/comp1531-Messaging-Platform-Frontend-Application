15b94d944fc7720124513ca19e96184d287e5a63
Ganesh <z5204758@ad.unsw.edu.au>
2023-03-10 16:18:46 +1100

    writing more test

--- src/channel.test.js
+++ src/channel.test.js
@@ -1,52 +1,61 @@
-<<<<<<< HEAD
-import {channelJoinV1} from './channel';
 import {channelsCreateV1} from ',/channels';
-import {authRegisterV1, authLoginV1} from './auth'; 
+import {authRegisterV1} from './auth'; 
+import {channelDetailsV1, channelMessagesV1, channelInviteV1,channelJoinV1} from './channel';
 import {getData, setData} from './dataStore';
+import {fromString} from 'uuid4';
 
 describe ("Testing channelJoinV1", () => {
-    const authRegisterId1 = authRegisterV1('lebron@gmail.com', '123456789','Lebron','James');
-    const authRegisterId2 = authRegisterV1('John@gmail.com', '123456789','John','Smith');
-    const channel1 = channelsCreateV1(authRegisterId1, 'firstChannel', true);
+    const authUserId = authRegisterV1('lebron@gmail.com', '123456789','Lebron','James');
+    const authUserId2 = authRegisterV1('John@gmail.com', '123456789','John','Smith');
+    //const authUserId3 = authRegisterV1('Kanye@hotmail.com', '123456789', 'Kanye', 'West' );
+    const channel1 = channelsCreateV1(authUserId, 'firstChannel', true);
 
-    afterEach(() => {
-        setData({ users: {}, channels: {} });
-    });
+    beforeEach (() => {
+        const data = {
+            users: { [authUserId]: {} },
+            channels: { [channel1.channelId]: channel1 },
+            
+        };
+        setData(data);
+    })
 
     //test with valid parameters if authorised user is added to channel
     test ('checks authorised user can join channel', () => {
-        channelJoinV1(authRegisterId2, channel1);
+        channelJoinV1(authUserId2, channel1);
         const data = getData();
         expect(data.channels).toEqual ({
             [channel1.channelId]: {
                 channelId: channel1.channelId,
                 name:'firstChannel',
                 isPublic: true,
-                OwnerIds: [authRegisterId1, authRegisterId2],
-                MemberIds: [authRegisterId1, authRegisterId2],
-                messages,
+                ownerMembers: [authUserId, authUserId2],
+                MemberIds: [authUserId, authUserId2],
             }
         }),
-
+    })
     //test if channelId does not refer to a valid channel
 
     //test if authUserId is invalid
+    test ('checks if authUserId is invalid', () => {
+        const invalidAuthUser = fromString(wrongInput);
+        channelJoinV1(invalidAuthUser, channel1);
+        const data = getData();
+        expect(data.channels).toEqual ({
+            [channel1.channelId]: {
+                channelId: channel1.channelId,
+                name:'firstChannel',
+                isPublic: true,
+                ownerMembers: [authUserId],
+                MemberIds: [authUserId],
+            }
+        })
+    })
 
     //test if authorised user is already a member of the channel
 
-    //
-    })
+})
 
-});
-
-
-
-
-
-=======
-import {channelDetailsV1, channelMessagesV1, channelInviteV1,channelJoinV1} from './channel.js';
-import {getData, setData } from './dataStore.js';
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //testing channelDetails:
 describe ("Testing channelDetails", () => {
     const authUserId = 'user123';
@@ -102,4 +111,3 @@ describe ("Testing channelDetails", () => {
     });
 
 });
->>>>>>> 19e7ea0fdbcf9277a8818a142dd4115d35160f7a
