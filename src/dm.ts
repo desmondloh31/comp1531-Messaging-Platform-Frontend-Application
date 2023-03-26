6b1148d5328f86aed127fc94f6f1caa5519becc2
Kavish <z5257649@ad.unsw.edu.au>
2023-03-26 18:47:09 +1100

    Documentation done

---
+++ src/dm.ts
@@ -0,0 +1,15 @@
+/**
+ * @module dm
+ * 
+ * @param {string} token - 
+ * @param {array} uIds - array of userIds
+ * 
+ * @returns {array} - dms: array of objects, where each object contains types { dmId, name }  
+ * 
+ * 
+ */
+
+/**
+ * 
+ * 
+ */
