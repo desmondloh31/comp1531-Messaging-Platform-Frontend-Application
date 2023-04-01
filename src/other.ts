9e3e23d60ed0f3b8994d9f906e9e9b03bfe29ea2
cs1531 <cs1531@cse.unsw.edu.au>
2023-04-01 22:03:11 +1100

    Results push

--- src/other.ts
+++
@@ -1,9 +0,0 @@
-import { setData } from "./dataStore";
-
-export function clearV1(){
-     setData({users: [],
-            channels: [],
-            dms: [],
-            msgcount: 0,})   
-            return {};   
-}
