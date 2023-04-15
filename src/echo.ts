e2929358d8df5fb41394c52aac272a2598ac9625
hritwik Nauriyal <h.nauriyal@student.unsw.edu.au>
2023-04-16 08:23:59 +1000

    Revert "Merge branch 'update-693a798384ff8d29bdd00ea59b229ea2651ae69d' into 'master'"
    This reverts merge request !122

--- src/echo.ts
+++ src/echo.ts
@@ -1,11 +1,6 @@
-import HTTPError from 'http-errors';
-
 function echo(value: string) {
   if (value === 'echo') {
-    // NEW Iteration 3
-    throw HTTPError(400, 'Cannot echo "echo"');
-    // OLD Iteration 2
-    // return { error: 'error' };
+    return { error: 'error' };
   }
   return value;
 }
